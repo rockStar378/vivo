65f6b9cc542ee64b9b4a88f78cf12e651dc4981a
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2026-04-13 04:01:23 +0530

    Update Youtube.py

--- VIVAANXMUSIC/platforms/Youtube.py
+++ VIVAANXMUSIC/platforms/Youtube.py
@@ -14,9 +14,9 @@ from pyrogram.types import Message
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
-from ShiviMusic import LOGGER
-from ShiviMusic.utils.database import is_on_off
-from ShiviMusic.utils.formatters import time_to_seconds
+from VIVAANXMUSIC import LOGGER
+from VIVAANXMUSIC.utils.database import is_on_off
+from VIVAANXMUSIC.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 from py_yt import VideosSearch
 logger = LOGGER(__name__)
